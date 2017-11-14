4fb359b950828f4368844701d84a947583ab4b36
Chris Talib <piks3l@mailoo.org>
2017-11-14 16:45:29 +0100

    updating the algo to return a letter

--- app/models/service.rb
+++ app/models/service.rb
@@ -8,6 +8,19 @@ class Service < ApplicationRecord
   def service_ratings
     total_ratings = points.map { |p| p.rating }
     avg = (total_ratings.sum.to_f) / (total_ratings.size.to_f)
-    avg.round
+    case avg.round
+    when 9..10
+      "A"
+    when 7..9
+      "B"
+    when 5..6
+      "C"
+    when 3..4
+      "D"
+    when 0..2
+      "F"
+    else
+      "No ratings yet"
+    end
   end
 end
