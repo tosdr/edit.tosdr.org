047eab88a09c53212b3bbdb576536b0ea6ba7992
Chris Talib <piks3l@mailoo.org>
2017-11-14 11:21:02 +0100

    [wip] points controller

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,22 +1,54 @@
 class PointsController < ApplicationController
   def index
-  end
-
-  def create
+    @points = Point.all
   end
 
   def new
+    if current_user
+      @point = Point.new
+    else
+      flash[:notice] = "Please log in or sign up to create a point."
+      redirect_to new_user_path
+    end
+  end
+
+  def create
+    @point = Point.new(point_params)
+    if @point.save
+      flash[:notice] = "You created a point!"
+      redirect_to points_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @point = Point.find(params[:id])
   end
 
   def show
+    @point = Point.find(params[:id])
   end
 
   def update
+    @point = Point.find(params[:id])
+    @point.update(point_params)
+    flash[:notice] = "Point successfully updated!"
+    redirect_to points_path
   end
 
   def destroy
+    @point = Point.find(params[:id])
+    @point.destroy
+    redirect_to points_path
+  end
+
+  private
+  def set_point
+    @point = Point.find(params[:point_id])
+  end
+
+  def point_params
+    params.require(:point).permit
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get 'points/index'
-
-  get 'points/create'
-
-  get 'points/new'
-
-  get 'points/edit'
-
-  get 'points/show'
-
-  get 'points/update'
-
-  get 'points/destroy'
-
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
