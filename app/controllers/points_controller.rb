d4dccfd2a9af30bdd8a500acc4078aa9cff1da94
Chris Talib <piks3l@mailoo.org>
2017-11-14 10:41:58 +0100

    creating points controller

---
+++ app/controllers/points_controller.rb
@@ -0,0 +1,22 @@
+class PointsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
