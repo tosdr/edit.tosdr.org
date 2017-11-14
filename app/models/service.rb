b089421792ad3ffd972a85e450aa93743c58d6df
Chris Talib <piks3l@mailoo.org>
2017-11-14 15:53:13 +0100

    migrating and implementing missing fields in db

--- app/models/service.rb
+++ app/models/service.rb
@@ -6,7 +6,7 @@ class Service < ApplicationRecord
   validates :url, presence: true
 
   def service_ratings
-    total_ratings = service.points.map { |p| p.rating }
+    total_ratings = points.map { |p| p.rating }
     avg = (total_ratings.sum.to_f) / (total_ratings.size.to_f)
     avg.round
   end
