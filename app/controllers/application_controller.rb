28ab9e0ee55e634375c2a174bd4a47ce767c6cee
Chris Talib <piks3l@mailoo.org>
2017-11-21 20:28:13 +0100

    enforcing ssl in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   def configure_permitted_parameters
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Enforce SSL in production
+  config.force_ssl = true
+
 end
