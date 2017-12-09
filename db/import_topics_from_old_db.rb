159b137d278969cc3d020522d30ee5799347d1c6
Michiel de Jong <michiel@ripple.com>
2017-12-09 07:36:50 +0100

    indentation

--- db/import_topics_from_old_db.rb
+++ db/import_topics_from_old_db.rb
@@ -6,13 +6,14 @@ Dir[File.join(Rails.root,"old_db/topics/*.json")].each do |json_file|
   imported_topic = Topic.new(
     title: hash['name'],
     subtitle: hash['subtitle'] || 'no text here',
-    description: hash['description'] || 'no text here')
-    imported_topic.save #add topic error handling
-    unless imported_topic.valid?
-      puts "### #{imported_topic.title} not imported ! ###"
-    else
-      puts "Imported and saved: #{imported_topic.title}"
-    end
-    puts "Exiting loop"
+    description: hash['description'] || 'no text here'
+    )
+  imported_topic.save #add topic error handling
+  unless imported_topic.valid?
+    puts "### #{imported_topic.title} not imported ! ###"
+  else
+    puts "Imported and saved: #{imported_topic.title}"
   end
-  puts "Finishing importing topics!"
+  puts "Exiting loop"
+end
+puts "Finishing importing topics!"
