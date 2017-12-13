44ac512698b6d4f21a7b6c5d1ff32be377b09346
Michiel de Jong <michiel@ripple.com>
2017-12-13 07:45:59 +0100

    feat: read JSON files

--- db/import_points_from_old_db.rb
+++ db/import_points_from_old_db.rb
@@ -1,24 +1,28 @@
 require 'json'
 require 'pry'
+require 'ApplicationRecord'
+require '../app/models/point'
 
-filepath_services = "../old_db/points/"
-filename = JSON.read(filepath_points)
+filepath_points = "../old_db/points/"
 
 puts "Importing points..."
-File.readlines(filename).each do |line|
+Dir.foreach(filepath_points) do |filename|
+  next if filename == '.' or filename == '..'
+  file = File.read(filepath_points + filename)
+  data = JSON.parse(file)
+  puts data
   imported_point = Point.new(
-    user_id: # go make the script look for the user, if no user, make it default
-    title: line['title'],
-    source: line['discussion'],
-    status: (line['needModeration'] == "true" ? "approved" : "pending"),
-    analysis: line['tldr']
-    rating: line['tosdr']['score'],
-
-    service = Service.find_by_name(line['services'])
-    binding.pry
-    service_id: service.id,
-    topic = Topic.find_by_title(line['topics']) #need to import the services first and match it by string
-    topic_id: topic.id, #need to import topics first and match it by string
+#   user_id: # go make the script look for the user, if no user, make it default
+    title: data['title'],
+    source: data['discussion'],
+    status: (data['needModeration'] == "false" ? "approved" : "pending"),
+    analysis: data['tldr'],
+    rating: data['tosdr']['score']
+#    service = Service.find_by_name(line['services'])
+#    binding.pry
+#    service_id: service.id,
+#    topic = Topic.find_by_title(line['topics']) #need to import the services first and match it by string
+#    topic_id: topic.id, #need to import topics first and match it by string
     )
   imported_point.save
 end
