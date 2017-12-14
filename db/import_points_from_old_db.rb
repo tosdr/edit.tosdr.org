a9bbe83b7d66736de2fe99bb7235b3d0af346c4e
Michiel de Jong <michiel@ripple.com>
2017-12-14 12:44:36 +0100

    WiP: https://github.com/tosdr/phoenix/issues/185#issuecomment-351657950

--- db/import_points_from_old_db.rb
+++ db/import_points_from_old_db.rb
@@ -1,31 +1,39 @@
 require 'json'
 require 'pry'
 
-# in repo root, run
+# in repo root, run:
 # rails runner db/import_points_from_old_db.rb
 
 filepath_points = "old_db/points/"
 
-puts "Importing points..."
-Dir.foreach(filepath_points) do |filename|
-  next if filename == '.' or filename == '..'
-  file = File.read(filepath_points + filename)
-  data = JSON.parse(file)
+def importPoint(data, service)
+  puts 'old data:'
   puts data
+  puts service
+  puts 'new data:'
   imported_point = Point.new(
-#   user_id: # go make the script look for the user, if no user, make it default
-    title: data['title'],
-    source: data['discussion'],
-    status: (data['needModeration'] == "false" ? "approved" : "pending"),
-    analysis: data['tldr'],
-    rating: data['tosdr']['score']
+    id: data['id'] + '-' + service,
+    title: data['title']
+    # service: service # todo: tell rails that this is a foreign key
+  )
+  puts imported_point
 #    service = Service.find_by_name(line['services'])
 #    binding.pry
 #    service_id: service.id,
 #    topic = Topic.find_by_title(line['topics']) #need to import the services first and match it by string
 #    topic_id: topic.id, #need to import topics first and match it by string
-    )
   imported_point.save
+  puts 'saved.'
+end
+
+puts "Importing points..."
+Dir.foreach(filepath_points) do |filename|
+  next if filename == '.' or filename == '..' or filename == 'README.md'
+  file = File.read(filepath_points + filename)
+  data = JSON.parse(file)
+  for i in 0 ... data['services'].size
+    importPoint(data, data['services'][i])
+  end
 end
 puts "Finishing importing points"
 puts "Done!"
--- db/import_topics_from_old_db.rb
+++ db/import_topics_from_old_db.rb
@@ -1,19 +1,27 @@
-require 'json'
+# in repo root, run:
+# rails runner db/import_topics_from_old_db.rb
 
-puts "Starting the loop and the import..."
-Dir[File.join(Rails.root,"old_db/topics/*.json")].each do |json_file|
-  hash = JSON.parse(File.read(json_file))
+filepath_topics = "old_db/topics/"
+
+def importTopic(data)
+  puts 'old data:'
+  puts data 
+  puts 'new data:'
   imported_topic = Topic.new(
-    title: hash['name'],
-    subtitle: hash['subtitle'] || 'no text here',
-    description: hash['description'] || 'no text here'
-    )
-  imported_topic.save #add topic error handling
-  unless imported_topic.valid?
-    puts "### #{imported_topic.title} not imported ! ###"
-  else
-    puts "Imported and saved: #{imported_topic.title}"
-  end
-  puts "Exiting loop"
+    title: data['title'],
+    subtitle: data['subtitle']
+  )
+  puts imported_topic
+  imported_topic.save
+  puts 'saved.'
 end
-puts "Finishing importing topics!"
+
+puts "Importing topics..."
+Dir.foreach(filepath_topics) do |filename|
+  next if filename == '.' or filename == '..' or filename == 'README.md'
+  file = File.read(filepath_topics + filename)
+  data = JSON.parse(file)
+  importTopic(data)
+end
+puts "Finishing importing topics"
+puts "Done!"
