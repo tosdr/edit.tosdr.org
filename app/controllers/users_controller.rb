756b3c15c7525763200e8d575a36d15fb43aa933
Chris Talib <piks3l@mailoo.org>
2017-12-15 07:27:53 +0100

    starting to configure the view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
+
   def show
+  end
+
+  def edit
+  end
+
+  def destroy
+    @user.destroy
+    flash[:notice] = "Your account has been deleted! Goodbye!"
+    redirect_to root_path
+  end
+
+  private
+  def set_user
     @user = User.find(params[:id])
   end
 end
