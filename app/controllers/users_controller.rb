bedd702f6b5187bd46abc6d5d34ce6c5ffdd0f1d
Chris Talib <piks3l@mailoo.org>
2017-12-15 07:40:02 +0100

    allowing user to cancel their account and modify their details, need some styling and link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user
 
-
   def show
   end
 
   def edit
-  end
-
-  def destroy
-    @user.destroy
-    flash[:confirmation] = "Are your sure you want to delete your account?"
-    flash[:notice] = "Your account has been deleted! Goodbye!"
-    redirect_to root_path
   end
 
   private
