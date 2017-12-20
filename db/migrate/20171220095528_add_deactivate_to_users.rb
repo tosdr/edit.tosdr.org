cd8f48af5a5eabb82d5aa9bc7b193e817d4ee95d
Chris Talib <piks3l@mailoo.org>
2017-12-20 11:21:47 +0100

    user are deactivated when cancelling the account some work still need to be done

--- db/migrate/20171220095528_add_deactivate_to_users.rb
+++ db/migrate/20171220095528_add_deactivate_to_users.rb
@@ -1,6 +1,6 @@
 class AddDeactivateToUsers < ActiveRecord::Migration[5.1]
   def change
-    remove_column :users, :deactivate
+    remove_column :users, :deactivated, :boolean, default: false
     add_column :users, :deactivated, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218193243) do
+ActiveRecord::Schema.define(version: 20171220095528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
