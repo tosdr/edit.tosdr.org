0db39304f30cae9e0132bf2ea221ca980c808837
Chris Talib <piks3l@mailoo.org>
2017-12-23 16:20:56 +0100

    added the wrong column to the wrong thing, fixing it

--- db/migrate/20171223150303_adding_privacy_related_to_services.rb
+++
@@ -1,5 +0,0 @@
-class AddingPrivacyRelatedToServices < ActiveRecord::Migration[5.1]
-  def change
-    add_column :services, :privacy_related, :boolean
-  end
-end
---
+++ db/migrate/20171223151948_adding_privacy_related_to_topics.rb
@@ -0,0 +1,4 @@
+class AddingPrivacyRelatedToTopics < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
