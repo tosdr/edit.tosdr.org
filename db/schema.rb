a15f8ffa0a5d3feaff957da53ef0b37ddcff5a1b
Chris Talib <piks3l@mailoo.org>
2017-12-23 16:24:10 +0100

    adding the view in the creation form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220095528) do
+ActiveRecord::Schema.define(version: 20171223151948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20171220095528) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "oldId"
+    t.boolean "privacy_related"
   end
 
   create_table "users", force: :cascade do |t|
