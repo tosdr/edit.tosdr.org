c4ab2ff50b8277eb9299809e8663f4aef21b3ff9
Chris Talib <piks3l@mailoo.org>
2017-12-23 17:27:35 +0100

    adding the new column, the view and correcting previous view on service that wasn't supposed to be there

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171223151948) do
+ActiveRecord::Schema.define(version: 20171223161343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20171223151948) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "grade"
+    t.string "wikipedia"
   end
 
   create_table "topics", force: :cascade do |t|
