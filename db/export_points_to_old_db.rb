623f1bddd39fa17fc9f5bc6bac2d477ab066776a
Michiel de Jong <michiel@ripple.com>
2018-01-04 14:50:25 +0100

    Services export script
    With the changes to old_db/services/*, this makes the import and export scripts compatible:
    
    rails db:drop db:create db:migrate ; rails runner db/import_services_from_old_db.rb ; rails runner db/export_services_to_old_db.rb

---
+++ db/export_points_to_old_db.rb
@@ -0,0 +1,30 @@
+# in repo root, run:
+# rails runner db/export_points_to_old_db.rb
+
+require 'json'
+
+filepath_points = "old_db/points/"
+counter = Date.today.to_time.to_i * 1000
+
+puts "Exporting points..."
+Point.all.each do |point|
+  puts point.to_json
+  puts point.service.to_json
+  if (point.oldId.nil?) then 
+    puts 'yes!'
+    puts counter
+    puts point.to_json
+    point.oldId = counter
+    counter = counter + 1
+  end
+  filename = point.oldId + '.json'
+  File.write(filepath_points + filename, JSON.pretty_unparse({
+    id: point.oldId,
+    title: point.title,
+    services: [
+      point.service.slug
+    ]
+  }))
+end
+puts "Finishing exporting points"
+puts "Done!"
---
+++ db/export_services_to_old_db.rb
@@ -0,0 +1,27 @@
+# in repo root, run:
+# rails runner db/export_services_to_old_db.rb
+
+require 'json'
+
+filepath_services = "old_db/services/"
+
+puts "Exporting services..."
+Service.all.each do |service|
+  puts service.to_json
+  filename = service.slug + '.json'
+  File.write(filepath_services + filename, JSON.pretty_unparse({
+    id: service.slug,
+    name: service.name,
+    keywords: service.keywords.split(','),
+    related: service.related.split(','),
+    urls: [ service.url ],
+    tosdr: {
+      rated: service.grade
+    },
+    meta: {
+      'spec-version': '1.1'
+    }
+  }))
+end
+puts "Finishing exporting services"
+puts "Done!"
