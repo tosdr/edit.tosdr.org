c139094d661b716b5acbc3ef5f3f219e39ff2886
Madeline OLeary <madeline.w.oleary@gmail.com>
2018-03-13 21:16:58 +0100

    added another submit button on the form page so that the redirect goes back to the form page in the event the user wants to create another point right away (there is probably a more elegant solution with ajax, am thinking about it)

--- db/migrate/20171214143221_create_comments.rb
+++ db/migrate/20171214143221_create_comments.rb
@@ -1,5 +1,6 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
+    drop_table :comments
     create_table :comments do |t|
       t.references :point, foreign_key: true
       t.string :summary
