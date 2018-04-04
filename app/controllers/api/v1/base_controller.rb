3636d52e6a85e78278ae7bafa203bd48364b7a91
Chris Talib <piks3l@mailoo.org>
2018-04-04 11:50:43 +0200

    [sec] protecting from csrf vuln in API scoping

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BaseController < ActionController::Base
+  protect_from_forgery
   include Pundit
-
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
 
