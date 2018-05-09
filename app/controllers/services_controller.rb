d8bda824e1984f7dc91bbbfeedcea69392b4e352
Chris Talib <piks3l@mailoo.org>
2018-05-09 17:52:34 +0200

    force auth for seeing all the services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,5 @@
 class ServicesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:show]
   before_action :set_curator, except: [:index, :show]
   before_action :set_service, only: [:show, :edit, :update, :destroy]
 
