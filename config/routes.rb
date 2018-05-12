24636db088ee5f5551dc623d2be00edcddd0796a
Chris Talib <piks3l@mailoo.org>
2018-05-12 22:21:11 +0200

    generated new view and controllers out of devise to customize them, some work on the views are need but it seems it was what was needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
-  get 'users/destroy', to: 'users#destroy'
-  devise_for :users
-  root to: 'pages#home'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }, only: [ :destroy ]
+
+  devise_for :users, controllers: {
+   :sessions => "users/sessions",
+   :registrations => "users/registrations" }
+
+   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'my_points', to: 'points#user_points', as: :my_points
