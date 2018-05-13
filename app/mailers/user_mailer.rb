aeda998779f94560995fd0d58e31c7f900673f89
Chris Talib <piks3l@mailoo.org>
2018-05-13 19:55:39 +0200

    changing the place of the message to make some formatting

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,13 +8,7 @@ class UserMailer < ApplicationMailer
   def welcome(user)
     @user = user
 
-    @greeting = "You're now a confirmed contributor of Terms of Service; Didn't Read crowdreading tool on https://edit.tosdr.org . If you haven't subscribed to this website, please contact the team at contact@tosdr.org .
-
- Please bear in mind that this version is a beta version that is still under development. You can follow the development on our github (https://github.com/tosdr/phoenix ) or on our blog (https://blog.tosdr.org ).
-
- It means that things can change, a lot. We will keep you updated if anything on our privacy policy or on our terms of services change.
-
-      Happy contributing!"
+    # @greeting =
 
     mail(to: @user.email, subject: 'Welcome')
   end
