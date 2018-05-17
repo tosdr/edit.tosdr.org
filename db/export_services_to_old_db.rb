6b82f2cf4f4ec1a28e7ff6fadd7cee6e7ba4fa79
Michiel de Jong <michielbdejong@users.noreply.github.com>
2018-05-17 11:15:54 +0200

    dont build none service (#352)

--- db/export_services_to_old_db.rb
+++ db/export_services_to_old_db.rb
@@ -16,54 +16,58 @@ puts "Exporting services..."
 Service.all.each do |service|
   # puts service.to_json
 
+  if service.slug != 'none'
+    filename = (service.slug || service.name.split('.').join('-')).downcase + '.json'
+    # File.delete(filepath_services + filename)
+    # filename = service.id.to_s + '.json'
+    begin
+      puts filename
+      file = File.read(filepath_services + filename)
+      puts 'YES'
+      data = JSON.parse(file)
+    rescue
+      puts 'NO'
+      data = {}
+      # puts 'new file ' + filename
+    end
+    data['tosdr'] = data['tosdr'] || {}
+    data['meta'] = data['meta'] || {}
 
-  filename = (service.slug || service.name.split('.').join('-')).downcase + '.json'
-  # File.delete(filepath_services + filename)
-  # filename = service.id.to_s + '.json'
-  begin
-    file = File.read(filepath_services + filename)
-    data = JSON.parse(file)
-  rescue
-    data = {}
-    # puts 'new file ' + filename
+    data['id'] = service.id
+    data['name'] = service.name
+    data['keywords'] = service.keywords ? service.keywords.split(',') : []
+    data['related'] = service.related ? service.related.split(',') : []
+    goodUrls = data['urls'] ? data['urls'].join(',') : ''
+    if service.url != goodUrls then
+      puts service.id.to_s + ' ' + service.url + ' <- ' + goodUrls
+    end
+    data['urls'] = service.url.split(',')
+    # data['tosdr']['rated'] = service.grade
+    data['meta']['spec-version'] = '1.1'
+
+    data['slug'] = (service.slug || service.name.split('.').join('-')).downcase
+
+    if (mapping['toId'][ (service.slug || service.name.split('.').join('-')).downcase ])
+      puts (service.slug || service.name.split('.').join('-')).downcase
+      puts mapping['toId'][ (service.slug || service.name.split('.').join('-')).downcase ]
+      puts service.id.to_s
+      panic()
+    end
+    mapping['toId'][ (service.slug || service.name.split('.').join('-')).downcase ] = service.id.to_s
+
+    if (mapping['toSlug'][service.id.to_s])
+      puts service.id.to_s
+      puts mapping['toSlug'][service.id.to_s]
+      puts (service.slug || service.name.split('.').join('-')).downcase
+      panic()
+    end
+    mapping['toSlug'][service.id.to_s] = (service.slug || service.name.split('.').join('-')).downcase
+
+    # migrate:
+    migratedFilename = service.id.to_s + '.json'
+    File.write(filepath_services + filename, JSON.pretty_unparse(data))
+    File.write(filepath_services_migrated + migratedFilename, JSON.pretty_unparse(data))
   end
-  data['tosdr'] = data['tosdr'] || {}
-  data['meta'] = data['meta'] || {}
-
-  data['id'] = service.id
-  data['name'] = service.name
-  data['keywords'] = service.keywords ? service.keywords.split(',') : []
-  data['related'] = service.related ? service.related.split(',') : []
-  goodUrls = data['urls'] ? data['urls'].join(',') : ''
-  if service.url != goodUrls then
-    puts service.id.to_s + ' ' + service.url + ' <- ' + goodUrls
-  end
-  data['urls'] = service.url.split(',')
-  # data['tosdr']['rated'] = service.grade
-  data['meta']['spec-version'] = '1.1'
-
-  data['slug'] = (service.slug || service.name.split('.').join('-')).downcase
-
-  if (mapping['toId'][ (service.slug || service.name.split('.').join('-')).downcase ])
-    puts (service.slug || service.name.split('.').join('-')).downcase
-    puts mapping['toId'][ (service.slug || service.name.split('.').join('-')).downcase ]
-    puts service.id.to_s
-    panic()
-  end
-  mapping['toId'][ (service.slug || service.name.split('.').join('-')).downcase ] = service.id.to_s
-
-  if (mapping['toSlug'][service.id.to_s])
-    puts service.id.to_s
-    puts mapping['toSlug'][service.id.to_s]
-    puts (service.slug || service.name.split('.').join('-')).downcase
-    panic()
-  end
-  mapping['toSlug'][service.id.to_s] = (service.slug || service.name.split('.').join('-')).downcase
-
-  # migrate:
-  migratedFilename = service.id.to_s + '.json'
-  File.write(filepath_services + filename, JSON.pretty_unparse(data))
-  File.write(filepath_services_migrated + migratedFilename, JSON.pretty_unparse(data))
 end
 File.write(filepath_services_mapping, JSON.pretty_unparse(mapping))
 
