ca7a697d09ac34fca6c06ea8f31159272207ed33
Michiel de Jong <michiel@ripple.com>
2018-05-24 11:36:13 +0200

    attempt to fix the db:seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,24 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "Cleaning up the database..."
+Reason.destroy_all
 Comment.destroy_all
-User.destroy_all
 Reason.destroy_all
 Point.destroy_all
 Service.destroy_all
 Case.destroy_all
 Topic.destroy_all
+User.destroy_all
 
 puts "Starts new seeding"
 test_user = User.new(email: "test@email.com", username: "test user", password: "testnonadminuser", password_confirmation: "testnonadminuser")
 test_user.save
+puts "You can log in with test@email.com / testnonadminuser"
 curator_test_user = User.new(email: "curatortest@email.com", password: "testcuratoruser", password_confirmation: "testcuratoruser", curator: true)
 curator_test_user.save
+puts "You can log in with curatortest@email.com / testcuratoruser"
 admin_test_user = User.new(email: "admintest@email.com", username: "admin test user", password: "testadminuser", password_confirmation: "testadminuser", admin: true)
 admin_test_user.save
+puts "You can log in with admintest@email.com / testadminuser"
 
-puts "Importing topics"
-load File.join(Rails.root,"db","import_topics_from_old_db.rb")
-puts "Importing services"
-load File.join(Rails.root,"db","import_services_from_old_db.rb")
-puts "Importing cases"
-load File.join(Rails.root,"db","import_cases_from_old_db.rb")
-puts "Importing points"
-load File.join(Rails.root,"db","import_points_from_old_db.rb")
-puts "Importing posts"
-load File.join(Rails.root,"db","import_posts_from_old_db.rb")
-puts "Done!"
+puts "Done! Now run 'rails server' and visit http://localhost:3000"
