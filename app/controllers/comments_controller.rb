e8b2232ee77286964de447f2d0b3e69d4f55336d
Michiel de Jong <michielbdejong@users.noreply.github.com>
2018-05-25 15:21:50 +0200

    fix #384: allow commenting on a point (#386)
    * fix #384: allow commenting on a point
    
    * clean up comments adding

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,30 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_point, only: [:new, :create]
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.point = @point
+
+    if @comment.save
+      flash[:notice] = "Comment added!"
+    else
+      flash[:notice] = "Error adding comment!"
+      puts @comment.errors.full_messages
+    end
+    redirect_to point_path(@point)
+  end
+
+  private
+
+  def set_point
+    @point = Point.find(params[:point_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:summary)
+  end
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :point
+
+  validates :summary, presence: true
 end
