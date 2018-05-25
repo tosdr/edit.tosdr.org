ed2689125682864036a7808f27f6db9c737b4323
Michiel de Jong <michiel@ripple.com>
2018-05-25 10:56:16 +0200

    document that only curators can add services

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,14 +20,15 @@ puts "Starts new seeding"
 test_user = User.new(email: "test@email.com", username: "test user", password: "testNonAdminUser1", password_confirmation: "testNonAdminUser1")
 test_user.save
 puts test_user.errors.full_messages
-puts "You can log in with test@email.com / testNonAdminUser1"
+# puts "You can log in with test@email.com / testNonAdminUser1"
 curator_test_user = User.new(email: "curatortest@email.com", password: "testCuratorUser1", password_confirmation: "testCuratorUser1", curator: true)
 curator_test_user.save
 puts curator_test_user.errors.full_messages
-puts "You can log in with curatortest@email.com / testCuratorUser1"
+# only curators (not admins) can add services:
+puts "You can log in with curatortest@email.com / testCuratorUser1 to add services, topics, cases, and points"
 admin_test_user = User.new(email: "admintest@email.com", username: "admin test user", password: "testAdminUser1", password_confirmation: "testAdminUser1", admin: true)
 admin_test_user.save
 puts admin_test_user.errors.full_messages
-puts "You can log in with admintest@email.com / testAdminUser1"
+# puts "You can log in with admintest@email.com / testAdminUser1"
 
 puts "Done! Now run 'rails server' and visit http://localhost:3000"
