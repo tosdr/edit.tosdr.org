22279651d5352a502a00f7848499a2fa72a690dc
Michiel de Jong <michiel@ripple.com>
2018-06-01 12:52:36 +0200

    add tosback quoting, wip draft

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180523100609) do
+ActiveRecord::Schema.define(version: 20180530131412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,11 @@ ActiveRecord::Schema.define(version: 20180523100609) do
     t.bigint "case_id"
     t.string "oldId"
     t.text "point_change"
+    t.string "quoteDoc"
+    t.string "quoteRev"
+    t.integer "quoteStart"
+    t.integer "quoteEnd"
+    t.string "quoteText"
     t.index ["case_id"], name: "index_points_on_case_id"
     t.index ["service_id"], name: "index_points_on_service_id"
     t.index ["topic_id"], name: "index_points_on_topic_id"
