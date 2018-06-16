80be2f3050790e02e9681d3910d6d9a7e523428c
Chris Talib <piks3l@mailoo.org>
2018-06-16 17:37:07 +0200

    [fix] fixing the search bar from a previous pr

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     if params[:query]
-      @services = Service.includes(:points).where('name ILIKE ?', "%#{params[:query]}%", "%#{params[:query]}%")
+      @services = Service.includes(:points).where('name ILIKE ?', "%#{params[:query]}%")
     else
       @services = Service.includes(:points).with_points
     end
