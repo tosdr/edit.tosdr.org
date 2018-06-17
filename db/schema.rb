3ea4e751e98ad4d8b2b445d8b4668efb4e13ec8f
Madeline OLeary <madeline.w.oleary@gmail.com>
2018-06-17 15:13:07 +0200

    dynamic description updates working -- need to ensure that user cannot submit without description, still looks not-pretty

--- db/schema.rb
+++ db/schema.rb
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(version: 20180530131412) do
     t.integer "score"
     t.string "title"
     t.text "description"
-    t.bigint "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "topic_id"
     t.index ["topic_id"], name: "index_cases_on_topic_id"
   end
 
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20180530131412) do
     t.string "quoteText"
     t.bigint "case_id"
     t.string "oldId"
+    t.text "reason"
     t.text "point_change"
     t.string "quoteDoc"
     t.string "quoteRev"
@@ -91,10 +92,13 @@ ActiveRecord::Schema.define(version: 20180530131412) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.string "status"
     t.string "wikipedia"
     t.string "keywords"
     t.string "related"
     t.string "slug"
+    t.index ["user_id"], name: "index_services_on_user_id"
   end
 
   create_table "topics", force: :cascade do |t|
@@ -139,7 +143,6 @@ ActiveRecord::Schema.define(version: 20180530131412) do
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
-  add_foreign_key "cases", "topics"
   add_foreign_key "comments", "points"
   add_foreign_key "points", "cases"
   add_foreign_key "points", "services"
@@ -147,4 +150,5 @@ ActiveRecord::Schema.define(version: 20180530131412) do
   add_foreign_key "points", "users"
   add_foreign_key "reasons", "points"
   add_foreign_key "reasons", "users"
+  add_foreign_key "services", "users"
 end
