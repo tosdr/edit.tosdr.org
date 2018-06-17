14d037c08067495d2b01ac42d42bf0908fb5f11b
Michiel de Jong <michiel@unhosted.org>
2018-06-17 10:52:32 +0200

    formatting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     @versions = Version.order("created_at DESC").limit(5) # .joins('INNER JOIN users ON "whodunnit"= cast(users."id" as text)')
     @versions.each do |version|
-      version.whodunnit = User.find(version.whodunnit).username || '(anonymous)'
+      version.whodunnit = User.find(version.whodunnit).username || '(User ' + version.whodunnit + ')'
     end
   end
 end
