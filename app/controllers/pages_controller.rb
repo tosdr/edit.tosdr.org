ac77c7cb8b6da4747fef062a6e16e1658d663d77
Michiel de Jong <michiel@unhosted.org>
2018-06-26 13:54:40 +0200

    Display whodunnit and title change in point versions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,5 @@ class PagesController < ApplicationController
   def home
     @services = Service.includes(:points).with_points_featured.sample(3)
     @versions = Version.order("created_at DESC").limit(5) # .joins('INNER JOIN users ON "whodunnit"= cast(users."id" as text)')
-    @versions.each do |version|
-      version.whodunnit = User.find(version.whodunnit).username || '(User ' + version.whodunnit + ')'
-    end
   end
 end
