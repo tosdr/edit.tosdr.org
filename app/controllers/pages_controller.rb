7b2e23579d40a51d9255e170c0083455966ec015
Michiel de Jong <michiel@unhosted.org>
2018-06-26 14:39:44 +0200

    display more recent changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
     @services = Service.includes(:points).with_points_featured.sample(3)
-    @versions = Version.order("created_at DESC").limit(5) # .joins('INNER JOIN users ON "whodunnit"= cast(users."id" as text)')
+    @versions = Version.order("created_at DESC").limit(50) # .joins('INNER JOIN users ON "whodunnit"= cast(users."id" as text)')
   end
 end
