96f26bcd931c0011ba6b97880ffd1689102c8dcb
Madeline OLeary <13802104+madoleary@users.noreply.github.com>
2018-07-06 10:00:04 +0200

    201805 case descriptions (#510)
    * dynamic description updates working -- need to ensure that user cannot submit without description, still looks not-pretty
    
    * no more stepped submission process, still problems with filling in the title and description dynamically
    
    * dynamic update for title, rating, description now working
    
    * fixed points controller to account for new unified form
    
    * made cases available to edit/update
    
    * topic and case required, form re-renders with errors if case is missing
    
    * Prevent committing database dumps
    
    * removed now irrelevant case_form partial
    
    * errors are rendering if no case, no topic now
    
    * initial points controller refactoring for update method
    
    * removed outdated ratings validations, refactored create method by creating private helper method
    
    * Update .gitignore

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,10 +1,10 @@
 class Point < ApplicationRecord
  has_paper_trail
  belongs_to :user, optional: true
- belongs_to :service
- belongs_to :topic
+ belongs_to :topic, optional: true
 
- belongs_to :case, optional: true
+ belongs_to :service
+ belongs_to :case
 
  has_many :comments, dependent: :destroy
 
@@ -13,10 +13,7 @@ class Point < ApplicationRecord
  validates :source, presence: true
  validates :status, inclusion: { in: ["approved", "pending", "declined", "disputed", "draft"], allow_nil: false }
  validates :analysis, presence: true
- validates :rating, presence: true
- validates :rating, numericality: true
-
- # before_save :check_changed_attributes_for_service_rating_update
+ validates :case_id, presence: true
 
 def self.search_points_by_multiple(query)
   Point.joins(:service).where("services.name ILIKE ? or points.status ILIKE ? OR points.title ILIKE ?", "%#{query}%", "%#{query}%", "%#{query}%")
