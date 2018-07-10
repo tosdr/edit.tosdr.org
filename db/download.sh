5797ad5001dd7057a83a30ff8e8805fc13d7a297
Michiel de Jong <michielbdejong@users.noreply.github.com>
2018-07-10 11:24:59 +0200

    List cases per topic (#538)
    * separate download.sh and export.sh
    
    * list cases per topic

--- db/download.sh
+++ db/download.sh
@@ -4,5 +4,3 @@ heroku pg:backups:download --app edit-tosdr-org
 mv latest.dump $DATE.dump
 pg_restore --verbose --clean --no-acl --no-owner -d phoenix_development $DATE.dump
 rails db:migrate
-rails runner db/export_points_to_old_db.rb
-rails runner db/export_services_to_old_db.rb
