0e05097420ad202eda7146ca01951bb15532d583
Michiel de Jong <michiel@unhosted.org>
2018-07-17 10:20:01 +0200

    re-index points when updating document text

--- db/import_from_tosback2.rb
+++ db/import_from_tosback2.rb
@@ -23,7 +23,7 @@ def importRules()
     site = ngxml.xpath("//sitename[1]/@name").to_s
     services = Service.where('url LIKE ?', "#{site}%")
     if (services.length != 1)
-      puts 'found ' + services.length.to_s + ' services for: ' + site
+      # puts 'found ' + services.length.to_s + ' services for: ' + site
     else
       puts 'service for ' + site + ' is ' + services[0].name
       ngxml.xpath("//sitename/docname").each do |doc|
@@ -32,13 +32,54 @@ def importRules()
         xpath = doc.at_xpath("./url/@xpath").to_s
         reviewed = (doc.at_xpath("./url/@reviewed").to_s == "true")
         text = File.read($crawls_path[reviewed] + site + $crawls_separator + name + $crawls_extension)
-        puts url
+        # puts url
         doc = Document.find_by_url(url)
         if (!doc)
           puts 'new: ' + url
           doc = Document.new()
+        else
+          if text.eql?(doc.text)
+            # puts 'document text unchanged!'
+          else
+            docPoints = doc.points
+            puts docPoints
+            docPoints.each do |p|
+              quoteStart = text.index(p.quoteText)
+              if (quoteStart.nil?)
+                comment = Comment.create({
+                  point: p,
+                  summary: 'Cannot find this point in the updated document, manual review required!'
+                })
+                if (comment.errors.full_messages.length > 0)
+                  puts comment.errors.full_messages
+                  panic()
+                end
+                p.status = 'pending'
+                if (p.case_id.nil?)
+                  p.case = Case.find_by_title('none')
+                end
+                if (!p.save())
+                  puts p.errors.full_messages
+                  panic()
+                end
+              else
+                quoteEnd = quoteStart + p.quoteText.length
+                if (quoteStart != p.quoteStart || quoteEnd != p.quoteEnd)
+                  puts 'reindexing point ' + p.id + ' (' + p.quoteStart + ' -> ' + quoteStart + ', ' +p.quoteEnd + ' -> ' + quoteEnd +  ')'
+                  p.update({
+                    quoteStart: quoteStart,
+                    quoteEnd: quoteEnd
+                  })
+                  if (!p.save())
+                    puts p.errors.full_messages
+                    panic()
+                  end
+                end
+              end
+            end
+          end
         end
-        puts 'updating'
+        # puts 'updating'
         doc.update({
           service: services[0],
           name: name,
@@ -47,7 +88,7 @@ def importRules()
           reviewed: reviewed,
           text: text
         })
-        puts 'saving'
+        # puts 'saving'
         if (!doc.save())
           puts doc.errors.full_messages
           panic()
