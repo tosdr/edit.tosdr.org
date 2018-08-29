d9f173c7dd91f96a7ba3a34ae4327cdd97905552
Michiel de Jong <michiel@unhosted.org>
2018-08-29 09:10:21 +0200

    pending points on home page for curators

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
 class PagesController < ApplicationController
   def home
-    @services = Service.includes(points: [:case]).with_points_featured.sample(3)
     @versions = Version.order("created_at DESC").limit(50) # .joins('INNER JOIN users ON "whodunnit"= cast(users."id" as text)')
+    if current_user&.curator
+      @points = Point
+                  .where(status: 'pending')
+                  .where.not(user_id: current_user.id)
+                  .includes(:service)
+                  .includes(:case)
+                  .includes(:user)
+                  .limit(20)
+    else
+      @services = Service.includes(points: [:case])
+                    .with_points_featured.sample(3)
+    end
   end
 end
