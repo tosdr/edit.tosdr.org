749d3f6f72402f6b2a120f5dae0546e3272412a0
Vincent <projects@vinnl.nl>
2018-08-29 15:15:08 +0200

    Highlight changes-requested and draft points
    Show a user's own points that are in draft mode or have changes
    requested on them by a curator on the homepage.
    
    Fixes #676.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,36 @@
 class PagesController < ApplicationController
   def home
     @versions = Version.order("created_at DESC").limit(50) # .joins('INNER JOIN users ON "whodunnit"= cast(users."id" as text)')
-    if current_user&.curator
-      @points = Point
+    if current_user&.curator then
+      @pending_points = Point
                   .where(status: 'pending')
                   .where.not(user_id: current_user.id)
                   .includes(:service)
                   .includes(:case)
                   .includes(:user)
-                  .limit(20)
-    else
+                  .limit(10)
+    end
+
+    if current_user then
+      @draft_points = Point
+                  .where(status: 'draft')
+                  .where(user_id: current_user.id)
+                  .includes(:service)
+                  .includes(:case)
+                  .includes(:user)
+                  .limit(10)
+
+      @change_request_points = Point
+                  .where(status: 'changes-requested')
+                  .where(user_id: current_user.id)
+                  .includes(:service)
+                  .includes(:case)
+                  .includes(:user)
+                  .limit(10)
+    end
+
+    # Only load example services for users who are not curators and don't have existing points to deal with.
+    if (!@pending_points && (!@draft_points || @draft_points.count == 0) && (!@change_request_points || @change_request_points.count == 0))
       @services = Service.includes(points: [:case])
                     .with_points_featured.sample(3)
     end
