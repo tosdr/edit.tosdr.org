ec8a9b0d99a271cfa921c77df7f8b37d9a8652f1
Madeline OLeary <madeline.w.oleary@gmail.com>
2018-10-14 17:18:53 +0200

    refactored home method so that the common home page only displays versions, like a newsfeed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,37 +1,12 @@
 class PagesController < ApplicationController
   def home
     @versions = Version.order("created_at DESC").limit(50) # .joins('INNER JOIN users ON "whodunnit"= cast(users."id" as text)')
-    if current_user&.curator then
-      @pending_points = Point
-                  .where(status: 'pending')
-                  .where.not(user_id: current_user.id)
-                  .includes(:service)
-                  .includes(:case)
-                  .includes(:user)
-                  .order(updated_at: :desc)
-                  .limit(10)
-    end
-
-    if current_user then
-      @draft_points = Point
-                  .where(status: 'draft')
-                  .where(user_id: current_user.id)
-                  .includes(:service)
-                  .includes(:case)
-                  .includes(:user)
-                  .limit(10)
-
-      @change_request_points = Point
-                  .where(status: 'changes-requested')
-                  .where(user_id: current_user.id)
-                  .includes(:service)
-                  .includes(:case)
-                  .includes(:user)
-                  .limit(10)
-    end
 
     # Only load example services for users who are not curators and don't have existing points to deal with.
-    if (!@pending_points && (!@draft_points || @draft_points.count == 0) && (!@change_request_points || @change_request_points.count == 0))
+    # Checks to see that current user does NOT have ANY points and checks to see that user is NOT a curator
+
+    if current_user.nil? || (!current_user.points.any? && !current_user.curator?)
+      @user_no_points = true
       @services = Service.includes(points: [:case])
                     .sample(3)
     end
