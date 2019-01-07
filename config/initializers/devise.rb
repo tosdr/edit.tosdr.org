c4fe1313b78bad7335a02b94ed293e44b1d9a6d2
christalib <piks3l@mailoo.org>
2019-01-07 12:13:47 +0100

    moving the configuration for emails to devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,10 +17,10 @@ Devise.setup do |config|
   config.mailer_sender = 'automatic-email@tosdr.org'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
-  # config.parent_mailer = 'ActionMailer::Base'
+  config.parent_mailer = 'ActionMailer::Base'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
