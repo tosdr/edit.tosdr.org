bbee5586503325cb178e8c7aa252571faa3ebb01
christalib <piks3l@mailoo.org>
2019-01-07 11:45:19 +0100

    running migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181101123536) do
+ActiveRecord::Schema.define(version: 20190107104259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,9 +98,9 @@ ActiveRecord::Schema.define(version: 20181101123536) do
     t.bigint "case_id"
     t.string "oldId"
     t.text "point_change"
+    t.boolean "service_needs_rating_update", default: false
     t.integer "quoteStart"
     t.integer "quoteEnd"
-    t.boolean "service_needs_rating_update", default: false
     t.bigint "document_id"
     t.index ["case_id"], name: "index_points_on_case_id"
     t.index ["document_id"], name: "index_points_on_document_id"
@@ -178,6 +178,10 @@ ActiveRecord::Schema.define(version: 20181101123536) do
     t.boolean "admin", default: false
     t.boolean "curator", default: false
     t.boolean "deactivated", default: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
