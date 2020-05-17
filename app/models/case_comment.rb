e7ef0024c141441c3337c340bd0c850754048709
Madeline OLeary <madeline.w.oleary@gmail.com>
2020-05-17 12:25:50 +0200

    making spammable available to comments models

--- app/models/case_comment.rb
+++ app/models/case_comment.rb
@@ -1,4 +1,5 @@
 class CaseComment < ApplicationRecord
   validates :summary, presence: true
   belongs_to :case
+  has_many :spams, as: :spammable
 end
--- app/models/document_comment.rb
+++ app/models/document_comment.rb
@@ -1,4 +1,5 @@
 class DocumentComment < ApplicationRecord
   validates :summary, presence: true
   belongs_to :document
+  has_many :spams, as: :spammable
 end
