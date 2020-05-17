52f64ef285c208885da6e915d64cab41d46fcc37
Madeline OLeary <madeline.w.oleary@gmail.com>
2020-05-17 12:26:56 +0200

    db changes for spam

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200405093018) do
+ActiveRecord::Schema.define(version: 20200517080230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,6 +143,16 @@ ActiveRecord::Schema.define(version: 20200405093018) do
     t.bigint "user_id"
     t.string "rating"
     t.index ["user_id"], name: "index_services_on_user_id"
+  end
+
+  create_table "spams", force: :cascade do |t|
+    t.string "spammable_type"
+    t.bigint "spammable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "flagged_by_admin_or_curator", default: false
+    t.boolean "cleaned", default: false
+    t.index ["spammable_type", "spammable_id"], name: "index_spams_on_spammable_type_and_spammable_id"
   end
 
   create_table "topic_comments", force: :cascade do |t|
