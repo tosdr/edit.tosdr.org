4b44cc261ff2b29ec4855892b0f41ee1d0fe9594
Michiel de Jong <michiel@unhosted.org>
2020-09-11 15:58:12 +0200

    My deploy script

---
+++ db/deploy.sh
@@ -0,0 +1,17 @@
+git checkout master
+git pull
+./db/download.sh
+./db/export.sh
+cd ../tosdr.org
+git checkout master
+git pull
+git add src
+git commit -am"export"
+./node_modules/.bin/grunt
+cp dist/api .
+git add dist
+git add api
+git commit -am"built"
+git push
+git subtree push --prefix dist 5apps master
+cd ../edit.tosdr.org
