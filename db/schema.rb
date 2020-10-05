488ef256577c1979da81a5e7c6e717046578fb81
Justin Back <jback@pixelcatproductions.net>
2020-10-06 00:57:21 +0200

    Fixed HTML position

--- db/schema.rb
+++ db/schema.rb
@@ -99,9 +99,9 @@ ActiveRecord::Schema.define(version: 20200517080230) do
     t.bigint "case_id"
     t.string "oldId"
     t.text "point_change"
+    t.boolean "service_needs_rating_update", default: false
     t.integer "quoteStart"
     t.integer "quoteEnd"
-    t.boolean "service_needs_rating_update", default: false
     t.bigint "document_id"
     t.index ["case_id"], name: "index_points_on_case_id"
     t.index ["document_id"], name: "index_points_on_document_id"
