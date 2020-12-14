f2f82633fb0794032ee33a481838905e0617b019
Michiel de Jong <michiel@unhosted.org>
2020-12-14 20:11:10 +0100

    try-catch in crawl_documents

--- db/crawl_documents.rb
+++ db/crawl_documents.rb
@@ -12,47 +12,49 @@ $userId = ENV['USER_ID']
 puts "Crawling documents.."
 Document.where(:id => $fromId..$toId, :status => nil).each do |document|
   puts 'Starting ' + document.id.to_s
-
-  puts 'crawling ' + document.url + ' (' + document.xpath + ')'
-
-  @tbdoc = TOSBackDoc.new({
-    url: document.url,
-    xpath: document.xpath
-  })
-
-  @tbdoc.scrape
-
-  oldLength = document.text.length
-  puts 'old length ' + oldLength.to_s
-  document.update(text: @tbdoc.newdata)
-  newLength = document.text.length
-  puts 'new length ' + newLength.to_s
-
-  if (newLength == 0 && document.xpath != nil)
-    puts 'Retrying without xpath'
-    @tbdoc2 = TOSBackDoc.new({
+  begin
+    puts 'crawling ' + document.url + ' (' + document.xpath + ')'
+    @tbdoc = TOSBackDoc.new({
       url: document.url,
-      xpath: nil
+      xpath: document.xpath
     })
-    @tbdoc2.scrape
-    if (@tbdoc2.newdata.length > 0)
-      puts 'Using this with length ' + @tbdoc2.newdata.length.to_s
-      newLength = @tbdoc2.newdata.length
-      document.update(text: @tbdoc2.newdata, xpath: nil)
-    end
-  end
-  @document_comment = DocumentComment.new()
-  @document_comment.summary = 'Crawled, old length: ' + oldLength.to_s + ', new length: ' + newLength.to_s
-  @document_comment.user_id = $userId
-  @document_comment.document_id = document.id
 
-  if @document_comment.save
-    puts "Comment added!"
-  else
-    puts "Error adding comment!"
-    puts @document_comment.errors.full_messages
+    @tbdoc.scrape
+
+    oldLength = document.text.length
+    puts 'old length ' + oldLength.to_s
+    document.update(text: @tbdoc.newdata)
+    newLength = document.text.length
+    puts 'new length ' + newLength.to_s
+
+    if (newLength == 0 && document.xpath != nil)
+      puts 'Retrying without xpath'
+      @tbdoc2 = TOSBackDoc.new({
+        url: document.url,
+        xpath: nil
+      })
+      @tbdoc2.scrape
+      if (@tbdoc2.newdata.length > 0)
+        puts 'Using this with length ' + @tbdoc2.newdata.length.to_s
+        newLength = @tbdoc2.newdata.length
+        document.update(text: @tbdoc2.newdata, xpath: nil)
+      end
+    end
+    @document_comment = DocumentComment.new()
+    @document_comment.summary = 'Crawled, old length: ' + oldLength.to_s + ', new length: ' + newLength.to_s
+    @document_comment.user_id = $userId
+    @document_comment.document_id = document.id
+
+    if @document_comment.save
+      puts "Comment added!"
+    else
+      puts "Error adding comment!"
+      puts @document_comment.errors.full_messages
+    end
+    puts 'Done ' + document.id.to_s
+  rescue
+    puts 'Fail ' _ document.id.to_s
   end
-  puts 'Done ' + document.id.to_s
   puts '---'
 end
 puts "Finished crawling documents!"
