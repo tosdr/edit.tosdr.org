0c155cbd040a887231041231bde6a1a23754ea11
Justin Back <jback@pixelcatproductions.net>
2020-12-15 15:23:35 +0100

    Permit deactivated in activeadmin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -11,5 +11,5 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-  permit_params :email, :username, :curator, :admin, :bot
+  permit_params :email, :username, :curator, :admin, :bot, :deactivated
 end
