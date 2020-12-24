8b900bf8879b8ed3a4a2a6bbabbeafa7d2ab540c
Justin Back <jback@pixelcatproductions.net>
2020-12-24 03:05:25 +0100

    Changed privacy policy, added cookie notice

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201215125020) do
+ActiveRecord::Schema.define(version: 2020_12_15_125020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
