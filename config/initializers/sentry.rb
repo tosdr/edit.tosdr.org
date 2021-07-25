ea00a790b7f173dd89baeaf13ba2ec7b1fc0c110
Justin René Back <justin.back@tosdr.org>
2021-07-25 06:15:34 +0200

    Add Staging Environment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,11 +1,6 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  if Rails.env.production?
-    config.environment = 'production'
-  else
-    config.environment = 'development'
-  end
-
-  config.traces_sample_rate = 0.1
+  config.environment = Rails.env
+  config.traces_sample_rate = 1.0
 end
