01568b3414d004b7ac48600f3bd449bb93c0bb8d
Madeline OLeary <madeline.w.oleary@gmail.com>
2023-05-08 09:40:53 +0100

    allows cors from h client

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    # to-do : dynamically add origin
     origins 'localhost:5000'
-    resource '/api/v1/points', headers: :any, methods: [:post]
+    resource '/api/v1/points', headers: :any, methods: %I[post patch delete]
+    resource '/api/v1/cases', headers: :any, methods: :get
   end
 end
