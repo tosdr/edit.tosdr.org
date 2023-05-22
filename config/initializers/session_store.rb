9702612649a4647322effee2a9adfa57b00988e2
Madeline OLeary <madeline.w.oleary@gmail.com>
2023-05-22 15:30:37 +0200

    feeds session store config correct arguments

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, 'h_key', domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: 'h_key', domain: :all, tld_length: 2
