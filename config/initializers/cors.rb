4053ea03ed72bebd1c0c8004a40eb28858066031
Madeline OLeary <madeline.w.oleary@gmail.com>
2023-05-31 12:39:23 +0200

    removes h_origin from cors policy, includes protocol on h_origin env variable

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # to-do : dynamically add origin
-    # ENV['H_ORIGIN'] || 'localhost:5000',
-    origins ENV['H_ORIGIN'], 'localhost:5000'
+    origins 'localhost:5000'
     resource '/api/v1/points', headers: :any, methods: %I[post patch delete]
     resource '/api/v1/cases', headers: :any, methods: :get
   end
