eff4c2a15e9ae5ba1c2942f6a722b0196646aa9c
Madeline OLeary <madeline.w.oleary@gmail.com>
2024-03-20 10:48:28 +0000

    implements pundit policy for topics

--- app/policies/topic_policy.rb
+++ app/policies/topic_policy.rb
@@ -1,7 +1,31 @@
 class TopicPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope.all
-    end
+  def index?
+    true
+  end
+
+  def show?
+    index?
+  end
+
+  def create?
+    (!user.nil? && user.admin?)
+  end
+
+  def edit?
+    create? || (!user.nil? && owner?)
+  end
+
+  def update?
+    edit?
+  end
+
+  def destroy?
+    edit?
+  end
+
+  private
+
+  def owner?
+    record.user.nil? ? (user.curator?) || (user.admin?) : (user == record.user)
   end
 end
