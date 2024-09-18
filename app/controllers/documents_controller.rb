90556a0d99a379f79324d7be763f080e2d44d777
Madeline OLeary <madeline.w.oleary@gmail.com>
2024-09-18 16:30:38 +0100

    implements ota crawler functionality, error handling for relevant methods

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -10,26 +10,12 @@ puts TOSBackDoc
 class DocumentsController < ApplicationController
   include Pundit::Authorization
 
-  PROD_CRAWLERS = {
-    "https://api.tosdr.org/crawl/v1": 'Random',
-    "https://api.tosdr.org/crawl/v1/eu": 'Europe (Recommended)',
-    "https://api.tosdr.org/crawl/v1/us": 'United States (Recommended)',
-    "https://api.tosdr.org/crawl/v1/eu-central": 'Europe (Central)',
-    "https://api.tosdr.org/crawl/v1/eu-west": 'Europe (West)',
-    "https://api.tosdr.org/crawl/v1/us-east": 'United States (East)',
-    "https://api.tosdr.org/crawl/v1/us-west": 'United States (West)'
-  }.freeze
-
-  DEV_CRAWLERS = {
-    "http://localhost:5000": 'Standalone (localhost:5000)',
-    "http://crawler:5000": 'Docker-Compose (crawler:5000)'
-  }.freeze
-
   before_action :authenticate_user!, except: %i[index show]
   before_action :set_document, only: %i[show edit update crawl restore_points]
   before_action :set_services, only: %i[new edit create update]
   before_action :set_document_names, only: %i[new edit create update]
-  before_action :set_crawlers, only: %i[new edit create update]
+  before_action :set_uri, only: %i[new edit create update crawl]
+  # before_action :set_crawlers, only: %i[new edit create update]
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -55,79 +41,22 @@ class DocumentsController < ApplicationController
     @document.user = current_user
     @document.name = @document.document_type.name if @document.document_type
 
-    # <ActionController::Parameters {"service_id"=>"4", "document_type_id"=>"2", "url"=>"https://opentermsarchive.org/en/privacy-policy", "selector"=>".TextContent_textContent__ToW2S", "crawler_server"=>"http://localhost:5000"} permitted: true>
-
-    # {
-    #   "name": "Open Terms Archive",
-    #   "documents": {
-    #     "Privacy Policy": {
-    #       "fetch": "https://opentermsarchive.org/en/privacy-policy",
-    #       "select": ".TextContent_textContent__ToW2S"
-    #     }
-    #   }
-    # }
-    service = Service.find(document_params[:service_id]).name
-    normalized_name = service.strip
-    document_type = DocumentType.find(document_params[:document_type_id]).name
-    
-
-    # declaration = {}
-    # declaration["name"] = normalized_name
-    # declaration["documents"] = {}
-    # declaration["documents"][document_type] = {}
-    # declaration["documents"][document_type]["fetch"] = document_params[:url]
-    # declaration["documents"][document_type]["selector"] = document_params[:selector]
-    # declaration = JSON.parse(JSON[declaration])
-
-    base_url = "http://localhost:3000/"
-    # url = 'https://docs.github.com/en/free-pro-team@latest/github/site-policy/github-privacy-statement'
-    url = document_params[:url]
-    # selector = '#article-contents'
+    document_url = document_params[:url]
     selector = document_params[:selector]
-    params = '{"fetch": "' + url + '","select": "' + selector + '"}'
 
-    # byebug
-    response = HTTParty.post(url, body: params)
+    request = build_request(document_url, @uri, selector)
+    results = fetch_text(request, @uri, @document)
 
-    puts '!!!!!!!!!!!!!!!!!!!!!!!!'
-    puts response.body
+    @document = results[:document]
+    message = results[:message]
 
-    parsed_response_body = JSON.parse(response.body)
-
-
-    puts parsed_response_body
-
-    # @document.text = 
-
-
-
-    # Net::HTTP implementation
-    #    puts "download and filter"
-    #    uri = URI("http://localhost:3000")
-    #    req = Net::HTTP::Post.new(uri) # => #<Net::HTTP::Post POST>
-    #    req.body = '{"fetch": "' + @url + '","select": "' + @xpath + '"}'
-    #    req.content_type = 'application/json'
-    #    res = Net::HTTP.start(hostname) do |http|
-    #      http.request(req)
-    #    end
-    #    puts res.code
-    #    puts res.content_type
-    #    puts res.body
-
-    # if @document.save
-    #   crawl_result = perform_crawl
-
-    #   unless crawl_result.nil?
-    #     if crawl_result['error']
-    #       flash[:alert] = crawler_error_message(crawl_result)
-    #     else
-    #       flash[:notice] = 'The crawler has updated the document'
-    #     end
-    #   end
-      # redirect_to document_path(@document)
-    # else
-    #   render :new
-    # end
+    if @document.save
+      flash[:notice] = message
+      redirect_to document_path(@document)
+    else
+      flash.now[:warning] = message.html_safe if message
+      render :new
+    end
   end
 
   def update
@@ -142,24 +71,24 @@ class DocumentsController < ApplicationController
     end
 
     # we should probably only be running the crawler if the URL or css selector have changed
-    run_crawler = @document.saved_changes.keys.any? { |attribute| %w[url selector crawler_server].include? attribute }
-    crawl_result = perform_crawl if run_crawler
+    run_crawler = @document.saved_changes.keys.any? { |attribute| %w[url selector].include? attribute }
 
-    if @document.save
-      # only want to do this if css selector or URL have changed
-      ## text is returned blank when there's a defunct URL or css selector
-      ### avoids server error upon 404 error in the crawler
-      # need to alert people if the crawler wasn't able to retrieve any text...
-      unless crawl_result.nil?
-        if crawl_result['error']
-          flash[:alert] = crawler_error_message(crawl_result)
-        else
-          flash[:notice] = 'The crawler has updated the document'
-        end
-      end
+    if run_crawler
+      request = build_request(@document.url, @uri, @document.selector)
+      results = fetch_text(request, @uri, @document)
+
+      @document = results[:document]
+      message = results[:message]
+      crawl_sucessful = results[:crawl_sucessful]
+    end
+
+    if crawl_sucessful && @document.save
+      flash[:notice] = 'Document updated!'
       redirect_to document_path(@document)
     else
-      render 'edit', locals: { crawlers: PROD_CRAWLERS }
+      message ||= 'Document failed to update'
+      flash.now[:warning] = message
+      render 'edit'
     end
   end
 
@@ -186,12 +115,26 @@ class DocumentsController < ApplicationController
 
   def crawl
     authorize @document
-    crawl_result = perform_crawl
-    if crawl_result['error']
-      flash[:alert] = crawler_error_message(crawl_result)
+
+    old_text = @document.text
+    request = build_request(@document.url, @uri, @document.selector)
+    results = fetch_text(request, @uri, @document)
+
+    @document = results[:document]
+    message = results[:message]
+    crawl_sucessful = results[:crawl_sucessful]
+
+    text_changed = old_text != @document.text
+
+    if crawl_sucessful && text_changed && @document.save
+      flash[:notice] = 'Crawl successful. Document text updated!'
+    elsif crawl_sucessful && !text_changed
+      flash[:notice] = 'Crawl successful. Document text unchanged.'
     else
-      flash[:notice] = 'The crawler has updated the document'
+      message ||= 'Crawl failed!'
+      flash.now[:warning] = message
     end
+
     redirect_to document_path(@document)
   end
 
@@ -227,92 +170,60 @@ class DocumentsController < ApplicationController
     @document_names = DocumentType.where(status: 'approved').order('name ASC')
   end
 
-  def set_crawlers
-    @crawlers = Rails.env.development? ? DEV_CRAWLERS : PROD_CRAWLERS
-  end
-
   def document_params
-    params.require(:document).permit(:service, :service_id, :user_id, :document_type_id, :name, :url, :selector, :crawler_server)
+    params.require(:document).permit(:service, :service_id, :user_id, :document_type_id, :name, :url, :selector)
   end
 
-  def crawler_error_message(result)
-    message = result['message']['name'].to_s
-    region = result['message']['crawler'].to_s
-    stacktrace = CGI::escapeHTML(result['message']['remoteStacktrace'].to_s)
-
-    `It seems that our crawler wasn't able to retrieve any text. <br><br>Reason: #{message} <br>Region: #{region} <br>Stacktrace: #{stacktrace}`
+  def set_uri
+    url = ENV['OTA_URL']
+    @uri = URI(url)
   end
 
-  # to-do: refactor out comment assembly
-  def perform_crawl
-    authorize @document
-    @tbdoc = TOSBackDoc.new({
-                              url: @document.url,
-                              xpath: @document.selector,
-                              server: @document.crawler_server
-                            })
+  def build_request(document_url, uri, selector)
+    request = Net::HTTP::Post.new(uri)
+    params = '{"fetch": "' + document_url + '","select": "' + selector + '"}'
+    request.body = params
+    request.content_type = 'application/json'
+    token = ENV['OTA_API_SECRET']
+    request['Authorization'] = "Bearer #{token}"
 
-    @tbdoc.scrape
-    @document_comment = DocumentComment.new
+    request
+  end
 
-    error = @tbdoc.apiresponse['error']
-    if error
-      message_name = @tbdoc.apiresponse['message']['name'] || ''
-      crawler = @tbdoc.apiresponse['message']['crawler'] || ''
-      stacktrace = @tbdoc.apiresponse['message']['remoteStacktrace'] || ''
-      @document_comment.summary = '<span class="label label-danger">Attempted to Crawl Document</span><br>Error Message: <kbd>' + message_name + '</kbd><br>Crawler: <kbd>' + crawler + '</kbd><br>Stacktrace: <kbd>' + stacktrace + '</kbd>'
-      @document_comment.user_id = current_user.id
-      @document_comment.document_id = @document.id
+  def fetch_text(request, uri, document)
+    crawl_sucessful = false
+
+    begin
+      response_text = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+        http.request(request)
+      end
+
+      case response_text
+      when Net::HTTPSuccess
+        puts 'HTTP Success'
+        response_body = response_text.body
+        parsed_response_body = JSON.parse(response_body)
+        document.text = parsed_response_body
+        crawl_sucessful = true
+        message = 'Document created!'
+      else
+        Rails.logger.error("HTTP Error: #{response.code} - #{response.message}")
+        message = "HTTP Error: Could not retrieve document text. Contact <a href='mailto:team@tosdr.org'>team@tosdr.org</a>. Details: #{response.code} - #{response.message}"
+      end
+    rescue SocketError => e
+      # Handle network-related errors
+      Rails.logger.error("Network Error: #{e.message}")
+      message = "Network Error: Crawler unreachable. Could not retrieve document text. Contact <a href='mailto:team@tosdr.org'>team@tosdr.org</a>. Details: #{e.message}"
+    rescue Timeout::Error => e
+      # Handle timeout errors
+      Rails.logger.error("Timeout Error: #{e.message}")
+      message = "Timeout Error:  Could not retrieve document text. Contact <a href='mailto:team@tosdr.org'>team@tosdr.org</a>. Details: #{e.message}"
+    rescue StandardError => e
+      # Handle any other standard errors
+      Rails.logger.error("Standard Error: #{e.message}")
+      message = "Standard Error: Could not retrieve document text. Contact <a href='mailto:team@tosdr.org'>team@tosdr.org</a>. Details: #{e.message}"
     end
 
-    document_blank = !@document.text.blank?
-    old_length = document_blank ? @document.text.length : 0
-    old_crc = document_blank ? Zlib.crc32(@document.text) : 0
-    new_crc = Zlib.crc32(@tbdoc.newdata)
-    changes_made = old_crc != new_crc
-
-    if changes_made
-      @document.update(text: @tbdoc.newdata)
-      new_length = @document.text ? @document.text.length : 'no text retrieved by crawler'
-
-      # There is a cron job in the crontab of the 'tosdr' user on the forum.tosdr.org
-      # server which runs once a day and before it deploys the site from edit.tosdr.org
-      # to tosdr.org, it will run the check_quotes script from
-      # https://github.com/tosdr/tosback-crawler/blob/225a74b/src/eto-admin.js#L121-L123
-      # So that if text has moved without changing, points are updated to the corrected
-      # quote_start, quote_end, and quote_text values where possible, and/or their status is
-      # switched between:
-      # pending <-> pending-not-found
-      # approved <-> approved-not-found
-      crawler = @tbdoc.apiresponse['message']['crawler'] || ''
-      @document_comment.summary = '<span class="label label-info">Document has been crawled</span><br><b>Old length:</b> <kbd>' + old_length.to_s + ' CRC ' + old_crc.to_s + '</kbd><br><b>New length:</b> <kbd>' + new_length.to_s + ' CRC ' + new_crc.to_s + '</kbd><br> Crawler: <kbd>' + crawler + '</kbd>'
-      @document_comment.user_id = current_user.id
-      @document_comment.document_id = @document.id
-    end
-
-    unless changes_made
-      @tbdoc.apiresponse['error'] = true
-      @tbdoc.apiresponse['message'] = {
-        'name' => 'The source document has not been updated. No changes made.',
-        'remoteStacktrace' => 'SourceDocument'
-      }
-    end
-
-    message_name = @tbdoc.apiresponse['message']['name'] || ''
-    crawler = @tbdoc.apiresponse['message']['crawler'] || ''
-    stacktrace = @tbdoc.apiresponse['message']['remoteStacktrace'] || ''
-
-    @document_comment.summary = '<span class="label label-danger">Attempted to Crawl Document</span><br>Error Message: <kbd>' + message_name + '</kbd><br>Crawler: <kbd>' + crawler + '</kbd><br>Stacktrace: <kbd>' + stacktrace + '</kbd>'
-    @document_comment.user_id = current_user.id
-    @document_comment.document_id = @document.id
-
-    if @document_comment.save
-      puts 'Comment added!'
-    else
-      puts 'Error adding comment!'
-      puts @document_comment.errors.full_messages
-    end
-
-    @tbdoc.apiresponse
+    { document: document, message: message, crawl_sucessful: crawl_sucessful }
   end
 end
