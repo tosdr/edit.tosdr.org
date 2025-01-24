6c5a6292157746b283a3ac3a54d4a057d95de81f
Madeline OLeary <madeline.w.oleary@gmail.com>
2025-01-24 17:34:40 +0000

    optimize services query on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,6 +21,6 @@ class PagesController < ApplicationController
     # Only load example services for users who are not curators and don't have existing points to deal with.
     no_drafts = !@draft_points || @draft_points.count.zero?
     no_change_requests = !@change_request_points || @change_request_points.count.zero?
-    @services = Service.includes(points: [:case]).sample(3) if !@pending_points && no_drafts && no_change_requests
+    @services = Service.includes(points: [:case]).order(Arel.sql('RANDOM()')).limit(3)
   end
 end
